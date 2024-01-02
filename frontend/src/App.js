c58874aac6c165159004507dae9fcac7115b5952
GrayOrchid <jorajostart@bk.ru>
2024-01-03 01:01:33 +0300

    1.51

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,4 @@
 import './ppp.css'
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { selectIsAuth } from './redux/reducers/authReducer';
-import { useNavigate } from 'react-router-dom';
 import RoutesComponent from './RoutesComponent';
 import Header from './components/nav/Header';
 
@@ -13,12 +9,6 @@ export default function App() {
     <div className='App'>
       <Header />
       <RoutesComponent />
-<<<<<<< Updated upstream
-
-      {/* {room?.name && (<RoomPage />)} */}
-
-=======
->>>>>>> Stashed changes
     </div >
   );
 }
--- frontend/src/components/nav/Header.jsx
+++ frontend/src/components/nav/Header.jsx
@@ -3,15 +3,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import './nav.css'
 import { FaReact } from "react-icons/fa";
 import { getMe, selectIsAuth } from '../../redux/reducers/authReducer';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import NavAuth from './NavAuth';
-import { motion } from 'framer-motion'
 import Title from '../title/Title';
 import User from './User';
 export default function Header() {
     let dispatch = useDispatch()
     let isAuth = useSelector(selectIsAuth)
-    let { room } = useSelector((state) => (state.room))
     let token = window.localStorage.getItem('token')
 
     useEffect(() => {
@@ -19,7 +17,7 @@ export default function Header() {
             dispatch(getMe())
         }
     }, []);
-    let location = useLocation()
+
     return (
         <header className='header'>
             <header className='header__nav'>
