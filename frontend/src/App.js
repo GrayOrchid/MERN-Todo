f9f1377d6cca1dd33604374fdef2a3f2bc17b2f1
GrayOrchid <jorajostart@bk.ru>
2024-01-09 19:50:58 +0300

    export health

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './ppp.css'
 import RoutesComponent from './RoutesComponent';
 import Header from './components/nav/Header';
+import { keepServerActive } from './CheckHealth';
 
 
 export default function App() {
+  setInterval(keepServerActive, 5 * 60 * 1000);
 
   return (
     <div className='App'>
--- frontend/src/CheckHealth.js
+++ frontend/src/CheckHealth.js
@@ -1,15 +1,13 @@
 import axios from './axios';
 
-async function keepServerActive() {
+export async function keepServerActive() {
+    console.log('Function keepServerActive is called.'); // Опционально: добавьте этот вывод для отладки
     try {
         await axios.get('/health');
-        console.log('Server is active');
+        console.log('Server is active'); // Опционально: вывод для отладки
     } catch (error) {
         console.error('There has been a problem with your request:', error);
     }
 }
 
 
-keepServerActive();
-
-setInterval(keepServerActive, 5 * 60 * 1000);
