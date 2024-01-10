518c088fa9d7e03af86d9909f06b49de3f61dc40
GrayOrchid <jorajostart@bk.ru>
2024-01-10 18:55:11 +0300

    One min

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { keepServerActive } from './CheckHealth';
 
 
 export default function App() {
-  setInterval(keepServerActive, 3 * 60 * 1000);
+  setInterval(keepServerActive, 1 * 30 * 1000);
 
   return (
     <div className='App'>
--- frontend/src/CheckHealth.js
+++ frontend/src/CheckHealth.js
@@ -1,10 +1,8 @@
 import axios from './axios';
 
 export async function keepServerActive() {
-    console.log('Function keepServerActive is called.');
     try {
         await axios.get('/health');
-        console.log('Server is active');
     } catch (error) {
         console.error('There has been a problem with your request:', error);
     }
